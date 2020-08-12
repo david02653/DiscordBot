35e16f9a2149f7229ca910b8a87cc2d5a9a3f2cb
david02653 <david02653@gmail.com>
2020-08-12 15:55:30 +0800

    remove token

--- src/main/java/david/msabot/discordbot/DiscordbotApplication.java
+++ src/main/java/david/msabot/discordbot/DiscordbotApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiscordbotApplication {
 
     public static void main(String[] args) throws Exception {
-        JDAService.createJDA("NzM3MjE0MjQ4OTYyNDkwNDE4.Xx6GRQ.9vXWpSN5LgxnqmDG1Ia1fCfwk4k");
+        JDAService.createJDA("");
 
         SpringApplication.run(DiscordbotApplication.class, args);
     }

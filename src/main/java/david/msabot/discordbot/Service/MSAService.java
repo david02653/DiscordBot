33bdb4ada03d4aa2159fff3eb6dc3ac0c19858dc
david02653 <david02653@gmail.com>
2020-11-16 04:13:22 +0800

    service arch

---
+++ src/main/java/david/msabot/discordbot/Service/MSAService.java
@@ -0,0 +1,59 @@
+package david.msabot.discordbot.Service;
+
+public class MSAService {
+
+    // Eureka service
+    public void serviceEnvironment(){
+        // implement action_service_env
+    }
+
+    // Zuul and swagger service
+    public void apiList(){
+        // implement action_service_api_list
+    }
+
+    // Zuul, swagger and Actuator service
+    public void usageInfo(){
+        // implement action_service_using_info
+    }
+
+    // Zuul and Actuator service
+    public void serviceInfo(){
+        // implement action_service_info
+    }
+
+    // Eureka service
+    public void healthData(){
+        // implement action_service_health
+    }
+
+    // Eureka service
+    public void connectError(){
+        // implement action_connect_error
+    }
+
+    // Jenkins service
+    public void buildFail(){
+        // implement action_build_fail
+    }
+
+    // bot using guide
+    public void botHelp(){
+        // implement action_bot_help
+    }
+
+    // action when new bot join
+    public void botJoin(){
+        // implement action_bot_join
+    }
+
+    // ask user for service
+    public void noService(){
+        // implement stage_no_service
+    }
+
+    // check if result has service name
+    public void checkIntent(){
+        // implement stage_check_intent
+    }
+}

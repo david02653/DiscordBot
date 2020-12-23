037f981f899e370f04ccf8629132510dc8429806
david02653 <david02653@gmail.com>
2020-12-24 02:58:40 +0800

    nothing

--- src/main/java/david/msabot/discordbot/Rabbitmq/RabbitConfig.java
+++ src/main/java/david/msabot/discordbot/Rabbitmq/RabbitConfig.java
@@ -147,6 +147,7 @@ public class RabbitConfig {
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(JENKINS_QUEUE);
         container.setMessageListener(jenkinsListener(new MessageHandler()));
+        // nothing
 
         return container;
     }
